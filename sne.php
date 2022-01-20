f3b01e81e28ec734806b4df25c4a4beed981a86f
metavers12 <98059880+metavers12@users.noreply.github.com>
2022-01-20 00:00:29 +0000

    Delete sne.php

--- sne.php
+++
@@ -1,7 +0,0 @@
-<?php     
-$to_email = 'name @ company . com';
-$subject = 'Testing PHP Mail';
-$message = 'This mail is sent using the PHP mail function';
-$headers = 'From: noreply @ company . com';
-mail($to_email,$subject,$message,$headers);
-?>
